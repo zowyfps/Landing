b23f60304949463f88a43228984414b04fe8e565
zowyfps <1rociozoe@gmail.com>
2022-08-31 21:13:52 -0300

    Desafio Contador con boton

---
+++ src/components/ItemCount/ItemCount.jsx
@@ -0,0 +1,28 @@
+import './style.css';
+
+import React,{useState, useEffect} from 'react';
+
+export const ItemCount = ({initial, stock, onAdd}) => {
+    const [count, setCount] = useState(initial);
+
+    const decrease = () => {
+        setCount(count - 1);
+    }
+
+    const increase = () => {
+        setCount(count + 1);
+    }
+
+  return (
+    <div className='counter'>
+        <button disabled={count <=1} onClick={decrease}>-</button>
+        <span>{count}</span>
+        <button disabled={count >= stock} onClick={increase}>+</button>
+        <div>
+            <button disabled={stock <= 0} onClick={() => onAdd(count)}>Agregar al carrito</button>
+        </div>
+    </div>
+  );
+}
+
+export default ItemCount;
--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,9 +1,16 @@
+import ItemCount from "../ItemCount/ItemCount";
 import React from "react";
 import Title from '../Title/Title';
 
 export const ItemListContainer = ({texto}) => {
+    const onAdd = (quantity) =>{
+         console.log(`Compraste ${quantity} unidades `);
+    }
     return (
+        <>
         <Title greeting={texto} />
+        <ItemCount initial={1} stock={5} onAdd={onAdd} />
+        </>
     );
 }
 
