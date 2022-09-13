5b3b4c72bea40d101e67c607d8b5a6c6cd048053
zowyfps <1rociozoe@gmail.com>
2022-09-12 23:34:22 -0300

    CartUpdate

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import './style.css';
-export const ItemDetail = ({data}) => {
+
+export const ItemDetail = ({ data }) => {
   return (
     <div className="container">
-      <div className="detail">
-        <img className='detail_image' src="{data.image}" alt="" />
-        <div className="content">
-          <h1>{data.title}</h1>
+        <div className="detail">
+            <img className='detail_image' src="{data.image}" alt="" />
+            <div className="content">
+                <h1>{data.title}</h1>
+            </div>
         </div>
-      </div>
     </div>
   );
 }
