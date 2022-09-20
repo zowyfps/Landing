28708cf31bb6e9a1e106151093fd9ce3dde472ec
zowyfps <1rociozoe@gmail.com>
2022-09-19 23:18:04 -0300

    ItemDetailUpdate

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -1,14 +1,19 @@
-import React, {useState} from 'react';
 import './style.css';
+
+import React, {useState} from 'react';
+import { useCartContext } from '../../context/CartContext';
 import ItemCount from "../ItemCount/ItemCount";
 import { Link } from 'react-router-dom';
 
-export const ItemDetail = ({ data }) => {
 
+export const ItemDetail = ({ data }) => {
   const [goToCart, setGoToCart] = useState(false);
+  
+  const {addProduct} = useCartContext();
 
   const onAdd = (quantity) =>{
     setGoToCart(true)
+    addProduct(data,quantity);
 }
 
   return (
