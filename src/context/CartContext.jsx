da85fa68ed90e42241fc4992c1c39d57b04c2b08
zowyfps <1rociozoe@gmail.com>
2022-09-19 23:15:57 -0300

    ContextFolder

---
+++ src/context/CartContext.jsx
@@ -0,0 +1,45 @@
+import React, { useState, useContext } from 'react';
+
+const CartContext = React.createContext([]);
+
+export const useCartContext = () => useContext(CartContext);
+
+const CartProvider = ({ children }) =>{
+    const [cart, setCart] = useState([]);
+
+    const addProduct = (item, quantity) => {
+        if (isInCart(item.id)){
+            setCart(cart.map(product => {
+                return product.id === item.id ? { ...product, quantity: product.quantity + quantity } : product
+            }))
+        } else {
+            setCart([...cart, { ...item, quantity }]);
+        }
+    }
+
+
+
+
+    const clearCart = () => setCart([]);
+
+    const isInCart = (id) => cart.find(product => product.id ===id) ? true : false;
+    
+    const removeProduct = (id) => setCart(cart.filter(product => product.id !==id));
+
+
+
+
+
+    return (
+        <CartContext.Provider value={{
+            clearCart,
+            isInCart,
+            removeProduct,
+            addProduct
+        }}>
+            {children}
+        </CartContext.Provider>
+    )
+}
+
+export default CartProvider
