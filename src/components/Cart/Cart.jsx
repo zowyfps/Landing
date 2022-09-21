441c85193a6fbbde5259ef5cd082d41f010f33e1
zowyfps <1rociozoe@gmail.com>
2022-09-21 20:03:33 -0300

    cart update

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
-
-export const Cart = () => {
+import { Link } from 'react-router-dom';
+import { useCartContext } from '../../context/CartContext';
+import ItemCart from '../ItemCart/ItemCart';
+ 
+const Cart = () => {
+  const { cart, totalPrice } = useCartContext();
+  
+  if (cart.length === 0){
+    return (
+      <>
+      <p>Carrito Vacio!</p>
+      <Link to='/'>Hacer Compras</Link>
+      </>
+    );
+  }
   return (
-    <div>Cart</div>
-  )
+    <>
+      {
+        cart.map(product => <ItemCart key={product.id} product={product} />)
+      }
+
+      <p>
+        total:{totalPrice()} 
+      </p>
+   
+    </>
+  );
 }
 
-export default Cart;
+export default Cart
--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -3,7 +3,10 @@ import './style.css';
 import { Link } from 'react-router-dom';
 import React from 'react';
 
+
 const Item = ({info}) => {
+
+  
     return (
       <Link to={`/detalle/${info.id}`} className="film">
           <img src={info.image} alt="" />
