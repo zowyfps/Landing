20be7cf2eacd8a787158f1da1ffbbddb04afdea5
zowyfps <1rociozoe@gmail.com>
2022-09-26 21:57:06 -0300

    desafio cart view arreglo

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css';
+import React from 'react';
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import Cart from './components/Cart/Cart';
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import NavBar from './components/NavBar/NavBar';
+import CartProvider from './context/CartContext';
 
 
 
@@ -13,6 +16,7 @@ function App() {
   return (
    <>
     <BrowserRouter>
+      <CartProvider>
       <NavBar />
       <Routes>
         <Route path='/' element={<ItemListContainer />} />
@@ -20,6 +24,7 @@ function App() {
         <Route path='/cart' element={<Cart />} />
         <Route path='/detalle/:detalleId' element={<ItemDetailContainer />} />
       </Routes>
+      </CartProvider>
     </BrowserRouter>  
    </>
   );
