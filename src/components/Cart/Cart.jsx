e9052f1460c12c330730008c13ca71864650158d
zowyfps <1rociozoe@gmail.com>
2022-09-29 15:28:59 -0300

    adddoc.orderscollection-desafiofirebase2

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,3 +1,4 @@
+import { addDoc, collection, getFirestore } from 'firebase/firestore';
 import React from 'react'
 import { Link } from 'react-router-dom';
 import { useCartContext } from '../../context/CartContext';
@@ -5,6 +6,25 @@ import ItemCart from '../ItemCart/ItemCart';
  
 const Cart = () => {
   const { cart, totalPrice } = useCartContext();
+
+  const order = {
+    buyer: {
+      name:'rociozoe',
+      email:'rociozoe@gmail.com',
+      phone:'1133851995',
+      addres:'dinamarca 1134',
+
+    },
+    items: cart.map(product => ({ id: product.id, title: product.title, price: product.price, quantity: product.quantity})),
+    total: totalPrice(),
+  }
+
+  const handleClick = () => {
+    const db = getFirestore();
+    const ordersCollection = collection(db, 'orders');
+    addDoc(ordersCollection, order)
+    .then (({ id }) => console.log(id))
+  }
   
   if (cart.length === 0){
     return (
@@ -23,7 +43,7 @@ const Cart = () => {
       <p>
         total:{totalPrice()} 
       </p>
-   
+      <button onClick={handleClick}>Emitir Compra</button>
     </>
   );
 }
